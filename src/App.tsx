56322e10675f742391b8f14acf6e9916fdc12046
pablogarc <juan.garo29si@gmail.com>
2023-12-19 19:27:55 -0600

    Add routing and components for main section and playlist item

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import MainSection from "./components/Sections/MainSection";
 import AsideMenu from "./components/AsideMenu";
+import PlaylistItem from "./pages/PlaylistItem";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 function App() {
+  const location = useLocation();
+
   return (
     <>
       <div id="app" className="relative h-screen p-2 gap-2">
@@ -9,13 +14,15 @@ function App() {
           <AsideMenu />
         </aside>
 
-        <main className="[grid-area:main] rounded-lg bg-zinc-900 overflow-y-auto">
-          Main
-        </main>
+          <main className="[grid-area:main] rounded-lg bg-zinc-900 overflow-y-auto">
+            <Routes location={location} key={location.pathname} >
+              <Route index element={<MainSection />} />
+              <Route path="/playlist/:id" element={<PlaylistItem />} />
+            </Routes>
+            <slot />
+          </main>
 
-        <footer className="[grid-area:player] min-h-[100px]">
-          footer
-        </footer>
+        <footer className="[grid-area:player] min-h-[100px]">footer</footer>
       </div>
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Router>
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
