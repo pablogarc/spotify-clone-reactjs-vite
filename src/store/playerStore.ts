0d8bcbc7affd430a25cd009e6b8b0d8de5c1cb02
pablogarc <juan.garo29si@gmail.com>
2023-12-21 18:42:06 -0600

    Refactor playerStore.ts to include new features

--- src/store/playerStore.ts
+++ src/store/playerStore.ts
@@ -1,24 +1,38 @@
 import { create } from "zustand";
+import { createJSONStorage, persist, devtools } from "zustand/middleware";
 
 interface UsePlayerStoreState {
-    isPlaying: boolean,
-    currentMusic: {
-      playlist?: any,
-      song?: any,
-      songs?: []
-    },
-    volume: number,
-    setVolume: Function,
-    setIsPlaying: Function,
-    setCurrentMusic: Function
-  }
-  
+  isPlaying: boolean;
+  currentMusic: {
+    playlist?: any;
+    song?: any;
+    songs: [];
+  };
+  currentTime: any;
+  volume: number;
+  setVolume: (by: number) => void;
+  setIsPlaying: (by: boolean) => void;
+  setCurrentMusic: (by: any) => void;
+  setCurrentTime: (by: any) => void;
+}
 
-export const usePlayerStore = create<UsePlayerStoreState>((set) => ({
-  isPlaying: false,
-  currentMusic: { playlist: null, song: null, songs: [] },
-  volume: 1,
-  setVolume: (volume: number) => set({ volume }),
-  setIsPlaying: (isPlaying: boolean) => set({ isPlaying }),
-  setCurrentMusic: (currentMusic: any) => set({ currentMusic }),
-}))
+export const usePlayerStore = create<UsePlayerStoreState>()(
+  devtools(
+    persist(
+      (set) => ({
+        isPlaying: false,
+        currentMusic: { playlist: null, song: null, songs: [] },
+        currentTime: 0,
+        volume: 1,
+        setVolume: (volume: number) => set({ volume }),
+        setIsPlaying: (isPlaying: boolean) => set({ isPlaying }),
+        setCurrentMusic: (currentMusic: any) => set({ currentMusic }),
+        setCurrentTime: (currentTime: any) => set({ currentTime }),
+      }),
+      {
+        name: "state",
+        storage: createJSONStorage(() => localStorage),
+      }
+    )
+  )
+);
