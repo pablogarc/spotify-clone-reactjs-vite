8b20136d96a41b15174d006175dad5f3c8ae91cd
pablogarc <juan.garo29si@gmail.com>
2023-12-30 15:33:34 -0600

    Add Link component to SideMenuItem

--- src/components/SideMenuItem.tsx
+++ src/components/SideMenuItem.tsx
@@ -1,6 +1,7 @@
 import { FiSearch } from "react-icons/fi";
 import { GrHomeRounded } from "react-icons/gr";
 import { VscLibrary } from "react-icons/vsc";
+import { Link } from "react-router-dom";
 
 interface SideMenuProps {
   href?: string;
@@ -17,14 +18,14 @@ const SideMenuItem = ({ href, text }: SideMenuProps) => {
   return (
     <>
       <li>
-        <a
-          href={href}
+        <Link
+          to={`${href}`}
           className="flex gap-4 text-zinc-400 hover:text-zinc-100 items-center py-3 px-5 font-medium transition duration-300"
         >
           {linksMap[text]}
           {text === "Library" ? "Your " + text : text}
           <slot />
-        </a>
+        </Link>
       </li>
     </>
   );
