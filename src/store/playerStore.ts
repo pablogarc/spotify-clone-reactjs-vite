f5985375141343ea9f936c1a63cceccb68a570cb
pablogarc <juan.garo29si@gmail.com>
2023-12-30 15:16:02 -0600

    Remove devtools and currentTime from playerStore.ts

--- src/store/playerStore.ts
+++ src/store/playerStore.ts
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { createJSONStorage, persist, devtools } from "zustand/middleware";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 interface UsePlayerStoreState {
   isPlaying: boolean;
@@ -8,31 +8,25 @@ interface UsePlayerStoreState {
     song?: any;
     songs: [];
   };
-  currentTime: any;
   volume: number;
   setVolume: (by: number) => void;
   setIsPlaying: (by: boolean) => void;
   setCurrentMusic: (by: any) => void;
-  setCurrentTime: (by: any) => void;
 }
 
 export const usePlayerStore = create<UsePlayerStoreState>()(
-  devtools(
     persist(
       (set) => ({
         isPlaying: false,
         currentMusic: { playlist: null, song: null, songs: [] },
-        currentTime: 0,
         volume: 1,
         setVolume: (volume: number) => set({ volume }),
         setIsPlaying: (isPlaying: boolean) => set({ isPlaying }),
         setCurrentMusic: (currentMusic: any) => set({ currentMusic }),
-        setCurrentTime: (currentTime: any) => set({ currentTime }),
       }),
       {
         name: "state",
         storage: createJSONStorage(() => localStorage),
       }
     )
-  )
 );
