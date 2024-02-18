8c59dec5ead6c7f1ecda4e35ad68b495ddc38280
pablogarc <juan.garo29si@gmail.com>
2024-02-17 20:19:51 -0600

    Fix aspect ratio in PlayListItemCard component

--- src/components/PlayListItemCard.tsx
+++ src/components/PlayListItemCard.tsx
@@ -21,7 +21,7 @@ const PlayListItemCard = ({ playlist }: SideMenuCardProps) => {
           to={`/playlist/${id}`}
           className="playlist-item transition-all duration-300 flex relative p-2 overflow-hidden gap-2 pb-6 rounded-md hover:bg-zinc-800 shadow-lg hover:shadow-xl bg-zinc-500/30 w-44 flex-col"
         >
-          <picture className=" aspect-square w-full h-auto flex-none">
+          <picture className="aspect-square w-full h-auto flex-none">
             <img
               src={cover}
               alt={`Cover of ${title} by ${artistsString}`}
